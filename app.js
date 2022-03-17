952f2666f87e5379233d95d2e8bea38e2b24fbe4
Ahmed Orabi <aorabi@outlook.com>
2022-03-17 19:25:15 +0300

    function + -

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello Node Js");
+//console.log("Hello Node Js");
+function sum(n1 , n2){
+
+return n1+n2 ;
+
+}
+
+function sub(n1 , n2){
+
+    return n1-n2 ;
+    
+    }
+
+
+    let op = process.argv[2] ;
+    let n1 = process.argv[3]*1;
+    let n2 = process.argv[4]*1;
+
+console.log(`${n1} + ${n2} = ${sum (n1,n2)}`);
+
+
+console.log(`${n1} - ${n2} = ${sub (n1,n2)}`);
+
+
+
